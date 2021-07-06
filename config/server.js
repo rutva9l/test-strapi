6040a1ae7fbb61287c7bce34d339f888526259cc
rutva9l <rutva943@gmail.com>
2021-07-07 02:01:31 +0530

    idek

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('MY_HEROKU_URL'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'cbcc7800b3ec7ee314ba4ffc258370ef'),
