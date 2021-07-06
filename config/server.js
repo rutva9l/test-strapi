4736fd6b79623c07b26cef4848c88d7a13a88da5
rutva9l <rutva943@gmail.com>
2021-07-06 21:23:12 +0530

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cbcc7800b3ec7ee314ba4ffc258370ef'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '50720f0c-adf7-412b-b354-1a2f96959d2f'
+};
